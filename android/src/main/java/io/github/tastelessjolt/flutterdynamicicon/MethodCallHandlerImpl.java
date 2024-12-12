c7934b84e552bbcadfe5426b78dbb1b28cb0aeb3
vysakh-compass <vysakh.prasanth@compassdigital.io>
2024-12-11 20:38:49 -0500

    Revert "android support"
    This reverts commit cc76db0e0c78ad65466ec00e90019a0411ce389d.

--- android/src/main/java/io/github/tastelessjolt/flutterdynamicicon/MethodCallHandlerImpl.java
+++ android/src/main/java/io/github/tastelessjolt/flutterdynamicicon/MethodCallHandlerImpl.java
@@ -2,32 +2,17 @@ package io.github.tastelessjolt.flutterdynamicicon;
 
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import android.content.Context;
-import android.content.pm.ActivityInfo;
 
 class MethodCallHandlerImpl implements MethodChannel.MethodCallHandler {
 
-  Context context;
-
-  public MethodCallHandlerImpl(Context c) {
-    context = c;
-  }
-
   @Override
   public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-
-if (call.method.equals("mSupportsAlternateIcons")) {
+    if (call.method.equals("mSupportsAlternateIcons")) {
       result.success(false);
-      result.success(true);
     } else if (call.method.equals("mGetAlternateIconName")) {
       result.error("Not supported", "Not supported on Android", null);
-      ActivityInfo activityInfo = IconChanger.getCurrentEnabledIcon(context);
-      result.success(Helper.getIconNameFromActivity(activityInfo.name));
     } else if (call.method.equals("mSetAlternateIconName")) {
       result.error("Not supported", "Not supported on Android", null);
-      String iconName = call.argument("iconName");
-      if(iconName == null) iconName = "MainActivity";
-      IconChanger.enableIcon(context, iconName);
     } else if (call.method.equals("mGetApplicationIconBadgeNumber")) {
       result.error("Not supported", "Not supported on Android", null);
     } else if (call.method.equals("mSetApplicationIconBadgeNumber")) {
@@ -37,4 +22,3 @@ if (call.method.equals("mSupportsAlternateIcons")) {
     }
   }
 }
-   
